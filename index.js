8bf3b190a70a677d500f25ed072abc6860d2705a
Elph <elphgeek@gmail.com>
2022-03-19 12:09:18 +0100

    fix: update dependencies

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ async function run() {
     const prTitle = `sync: ${opt.fromBranch} to ${opt.toBranch}`;
     const prBody = `New code has just landed in \`${opt.fromBranch}\`, so let's bring \`${opt.toBranch}\` up to speed!`;
     
-    console.log(`Making a pull request to '${opt.toBranch}' from '${opt.fromBranch}' using branch '${opt.prBranchName}'. wow!`);
+    console.log(`Making a pull request to '${opt.toBranch}' from '${opt.fromBranch}' using branch '${opt.prBranchName}'.`);
 
     const {
       payload: { repository }
     } = github.context;
 
-    const octokit = new github.GitHub(opt.githubToken);
+    const octokit = github.getOctokit(opt.githubToken);
 
-    const { data: currentPulls } = await octokit.pulls.list({
+    const { data: currentPulls } = await octokit.rest.pulls.list({
       owner: repository.owner.name,
       repo: repository.name
     });
@@ -42,20 +42,20 @@ async function run() {
 
     if (!currentPull) {
 
-      const { data: fromRef } = await octokit.git.getRef({
+      const { data: fromRef } = await octokit.rest.git.getRef({
         owner: repository.owner.login,
         repo: repository.name,
         ref: `heads/${opt.fromBranch}`,
       });
 
-      await octokit.git.createRef({
+      await octokit.rest.git.createRef({
         owner: repository.owner.login,
         repo: repository.name,
         ref: `refs/heads/${opt.prBranchName}`,
         sha: fromRef.object.sha
       });
 
-      const { data: pullRequest } = await octokit.pulls.create({
+      const { data: pullRequest } = await octokit.rest.pulls.create({
         owner: repository.owner.login,
         repo: repository.name,
         head: opt.prBranchName,
@@ -72,7 +72,7 @@ async function run() {
       if(opt.reviewers.length > 0) {
 
         // the call in octokit rest requestReviewers seems not to exist, so its calling directly the method
-        await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers', {
+        await octokit.rest.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers', {
           owner: repository.owner.login,
           repo: repository.name,
           pull_number: pullRequest.number,
@@ -92,7 +92,7 @@ async function run() {
         `You can view it here: ${currentPull.url}`
       );
 
-      await octokit.repos.merge({
+      await octokit.rest.repos.merge({
         owner: repository.owner.login,
         repo: repository.name,
         base: opt.prBranchName,
